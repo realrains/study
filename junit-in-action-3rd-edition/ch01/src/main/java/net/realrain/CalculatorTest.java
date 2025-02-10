6538a341f86e4177ef38f3768de8e2b7e5f815e4
realrain <real.longrain@gmail.com>
2025-02-10 15:17:09 +0900

    1.3

--- junit-in-action-3rd-edition/ch01/src/main/java/net/realrain/CalculatorTest.java
+++ junit-in-action-3rd-edition/ch01/src/main/java/net/realrain/CalculatorTest.java
@@ -1,11 +1,29 @@
 package net.realrain;
 
 public class CalculatorTest {
-    public static void main(String[] args) {
+
+    private int nbErrors = 0;
+
+    private static void testAdd() {
         Calculator calculator = new Calculator();
         double result = calculator.add(10, 50);
         if (result != 60) {
-            System.out.println("Bad result: " + result);
+            throw new IllegalStateException("Bad result: " + result);
+        }
+    }
+
+    public static void main(String[] args) {
+        CalculatorTest test = new CalculatorTest();
+
+        try {
+            test.testAdd();
+        } catch (Throwable e) {
+            test.nbErrors++;
+            e.printStackTrace();
+        }
+
+        if (test.nbErrors > 0) {
+            throw new IllegalStateException("There were " + test.nbErrors + "error(s)");
         }
     }
 }
