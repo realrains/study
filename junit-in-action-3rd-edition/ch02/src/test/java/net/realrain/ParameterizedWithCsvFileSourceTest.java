5ea37363c31435fc9ab5068bd67cbc0d6580b6f3
realrain <real.longrain@gmail.com>
2025-02-11 01:22:39 +0900

    2.22

---
+++ junit-in-action-3rd-edition/ch02/src/test/java/net/realrain/ParameterizedWithCsvFileSourceTest.java
@@ -0,0 +1,17 @@
+package net.realrain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParameterizedWithCsvFileSourceTest {
+    private WordCounter wordCounter = new WordCounter();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/word_counter.csv")
+    void testWordsInSentence(int expected, String sentence) {
+        assertEquals(expected, wordCounter.countWords(sentence));
+    }
+
+}
